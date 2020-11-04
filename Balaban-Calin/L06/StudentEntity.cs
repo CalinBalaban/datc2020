7fafe2279ce816e0e8375c97aed8fa289ebbf034
Calin Balaban <calinbalaban@gmail.com>
2020-11-04 14:36:28 +0200

    tema6

---
+++ Balaban-Calin/L06/StudentEntity.cs
@@ -0,0 +1,21 @@
+using Microsoft.WindowsAzure.Storage.Table;
+	
+namespace Models
+{
+    public class Studententity : TableEntity
+    {
+        public Studententity(string Universitate, string Cnp)
+        {
+            this.PartitionKey = Universitate;
+            this.RowKey = Cnp;
+        }
+        public Studententity() {}
+
+        public string Nume {get; set;}
+        public string Prenume {get; set;}
+        public string Facultate {get; set;}
+        public int An {get; set;}
+        
+
+    }
+}
